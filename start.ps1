65061ea96d990202044d5736abfd72fcfd4e9e45
doughjoe77 <doughjoe77@hotmail.com>
2025-06-10 09:30:11 -0400

    also updated start.ps1 to force auth to DDN if it's not currently authenticated

--- start.ps1
+++ start.ps1
@@ -1,10 +1,13 @@
+# check to see if you are logged in to the DDN, if you are not logged in you
+# will be prompted to login to get your PAT
+ddn auth login
 $Env:HASURA_DDN_PAT = ddn auth print-access-token
 $Env:PROMPTQL_SECRET_KEY = ddn auth print-promptql-secret-key
+
 # bring up the data sources
 docker compose -f ./domain-services/datasource/compose.yaml --env-file ./domain-services/datasource/.env up -d
 
 # bring up the DDN and it's components
-#docker compose -f ./domain-services/ddn/compose.yaml --env-file ./domain-services/ddn/.env up -d
 Push-Location "./domain-services/ddn"
 ddn run docker-start -- -d
 Pop-Location
